13bba2a4a2b7e1519c1387124f38ec65501de523
Killua <unhunsowhat_nini@hotmail.com>
2019-04-27 23:29:06 +0800

    hw2_done

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,9 @@
 function capitalize(str) {
-  return str;
+  let newStr = str;
+  newStr = newStr.split('');
+  newStr[0] = newStr[0].toUpperCase();
+  newStr = newStr.join('');
+  return newStr;
 }
 
 console.log(capitalize('hello'));
