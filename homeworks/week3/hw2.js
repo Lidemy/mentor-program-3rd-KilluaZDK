eb168d9290c6c1c857e6aaeb02f780fb4a4cbd0b
Killua <unhunsowhat_nini@hotmail.com>
2019-05-04 00:46:11 +0800

    hw2_done

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,23 +1,10 @@
 function alphaSwap(str) {
-  // let newStr = '';
-  // const upperStr = str.toUpperCase();
-  // for (let i = 0; i < str.length; i += 1) {
-  //   str[i] === upperStr[i] ? newStr += str[i].toLowerCase() : newStr += upperStr[i];
-  // }
-  // return newStr;
-
-
   let newStr = '';
   const upperStr = str.toUpperCase();
   for (let i = 0; i < str.length; i += 1) {
-    if (str[i] === upperStr[i]) {
-      newStr += str[i].toLowerCase();
-    } else {
-      newStr += upperStr[i];
-    }
+    newStr += str[i] === upperStr[i] ? str[i].toLowerCase() : upperStr[i];
   }
   return newStr;
 }
-
 
 module.exports = alphaSwap;
