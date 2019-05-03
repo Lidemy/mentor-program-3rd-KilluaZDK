599c06849d3d00b235e1b03aa2f49d13d4d6337d
Killua <unhunsowhat_nini@hotmail.com>
2019-05-04 01:48:58 +0800

    test_done

--- homeworks/week3/hw4.test.js
+++ homeworks/week3/hw4.test.js
@@ -1,7 +1,19 @@
 const isPalindromes = require('./hw4');
 
 describe('hw4', () => {
-  it('should return correct answer when str = abcdcba', () => {
+  test('should return correct answer when str = abcdcba', () => {
     expect(isPalindromes('abcdcba')).toBe(true);
+  });
+  test('should return correct answer when str = abcba', () => {
+    expect(isPalindromes('abcba')).toBe(true);
+  });
+  test('should return correct answer when str = apple', () => {
+    expect(isPalindromes('apple')).toBe(false);
+  });
+  test('should return correct answer when str = aaaaa', () => {
+    expect(isPalindromes('aaaaaa')).toBe(true);
+  });
+  test('should return correct answer when str = applppa', () => {
+    expect(isPalindromes('applppa')).toBe(true);
   });
 });
